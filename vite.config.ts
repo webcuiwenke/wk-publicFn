7469cba76e8bd5ceb775dde7a8a226d57732080e
cuiwenke <1045414997@qq.com>
2024-02-29 14:36:18 +0800

    指定端口

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,14 @@ export default defineConfig({
           //   },
           // },
         },
-       
+
       ],
     }),
   ],
+  server: {
+    host: true,
+    port: 3333,
+  },
   build: {
     lib: {
       entry: './src/myLibrary.js', // 指定你的JS入口文件
