60ac159638b3446bd8e0ef6825e66bd3694c4aa7
cuiwenke <1045414997@qq.com>
2024-03-07 11:05:11 +0800

    修正配置

--- src/my-publicFn.ts
+++
@@ -1,9 +0,0 @@
-import { deepArrErgodic, findParentInfo, findPropertyInArray } from "./modules/ArrayFn.ts";
-import { objCopy } from "./modules/ObjectFn.ts";
-import { setCookie, getCookie } from "./modules/other.ts";
-export default {
-    deepArrErgodic, findParentInfo, findPropertyInArray,
-    objCopy,
-    setCookie,
-    getCookie,
-}
--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,17 @@ import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 export default defineConfig({
   plugins: [
-    createHtmlPlugin({
-      minify: true,
-      pages: [
-        {
-          entry: 'src/my-publicFn',
-          filename: 'index.html',
-          template: '/index.html',
-          // injectOptions: {
-          //   data: {
-          //     title: 'index',
-          //     injectScript: `<script src="./inject.js"></script>`,
-          //   },
-          // },
-        },
+    // createHtmlPlugin({
+    //   minify: true,
+    //   pages: [
+    //     {
+    //       entry: 'src/index.ts',
+    //       filename: 'index.html',
+    //       template: '/index.html',
+    //     },
 
-      ],
-    }),
+    //   ],
+    // }),
   ],
   server: {
     host: true,
@@ -26,9 +20,9 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: './src/my-publicFn.ts', // 指定你的JS入口文件
-      name: 'my-publicFn', // 打包后暴露的全局变量名
-      fileName: (format: string) => `my-publicFn.${format}.js`, // 输出的文件名格式
+      entry: './src/index.ts', // 指定你的JS入口文件
+      name: 'js-public-fn', // 打包后暴露的全局变量名
+      fileName: (format: string) => `js-public-fn.${format}.js`, // 输出的文件名格式
     },
     rollupOptions: {
       // external: ['dependency'], // 如果你的库依赖了外部库，如'dependency'，需要在这里声明
